1978e530bc0b95305c0cb94ef2682b52706a76cd
Tijmen Verhoef <tijmenmenno@gmail.com>
2026-01-27 16:38:40 +0100

    adds wave manager that spawns enemies for a fixed number of waves

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,13 +1,25 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Game Settings")]
     [SerializeField]
-    EnemyRuntimeSet _enemyRuntimeSet = null;
+    float _gameOverTimeout = 1.0F;
+    [Header("Player Settings")]
+    [SerializeField]
+    PlayerCharacter _playerCharacter = null;
+    [SerializeField]
+    List<Transform> _playerSpawnPoints = new List<Transform>();
 
     bool _gameFinished = false;
+
+    void Start()
+    {
+        SpawnPlayer();
+    }
 
     void Update()
     {
@@ -17,15 +29,28 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void SpawnPlayer()
+    {
+        Transform spawnTransform = transform;
+        if (_playerSpawnPoints.Count > 0)
+        {
+            spawnTransform = _playerSpawnPoints[Random.Range(0, _playerSpawnPoints.Count)];
+        }
+
+        PlayerCharacter newCharacter = Instantiate(_playerCharacter);
+        newCharacter.transform.position = spawnTransform.position;
+        newCharacter.transform.rotation = spawnTransform.rotation;
+    }
+
     private bool IsGameOver()
     {
-        return _enemyRuntimeSet.items.Count == 0;
+        return false;
     }
 
     private IEnumerator OnGameOverCoroutine()
     {
         _gameFinished = true;
-        yield return new WaitForSeconds(5.0F);
+        yield return new WaitForSeconds(_gameOverTimeout);
         SceneManager.LoadSceneAsync("GameOverScene", LoadSceneMode.Single);
     }
 }
---
+++ Assets/Scripts/Gameplay/WaveManager.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [Header("Runtime Data")]
+    [SerializeField]
+    EnemyRuntimeSet _enemyRuntimeSet = null;
+
+    [Header("Wave Settings")]
+    [SerializeField]
+    int _waveCount = 1;
+    [SerializeField]
+    int _enemiesPerWave = 1;
+    [SerializeField]
+    EnemyCharacter _enemyCharacter = null;
+    [SerializeField]
+    List<Transform> _enemySpawnPoints = new List<Transform>();
+
+    int _waveCounter = 0;
+    int _enemiesLeftToSpawn = 0;
+
+    void Update()
+    {
+        // All enemies defeated, start next wave
+        if (IsWaveOver())
+        {
+            _waveCounter += 1;
+            _enemiesLeftToSpawn = _enemiesPerWave;
+        }
+
+        // Only spawn enemies if waves are not yet done
+        if (!WavesFinished())
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private bool WavesFinished()
+    {
+        return _waveCounter > _waveCount;
+    }
+
+    private bool IsWaveOver()
+    {
+        return _enemiesLeftToSpawn == 0 && _enemyRuntimeSet.items.Count == 0;
+    }
+
+    private void SpawnEnemy()
+    {
+        // TODO(nemjit001): Limit number of enemies at the same time or add time between spawns
+        if (_enemiesLeftToSpawn == 0)
+        {
+            return;
+        }
+
+        Transform spawnTransform = transform;
+        if (_enemySpawnPoints.Count > 0)
+        {
+            spawnTransform = _enemySpawnPoints[Random.Range(0, _enemySpawnPoints.Count)];
+        }
+
+        EnemyCharacter newEnemy = Instantiate(_enemyCharacter);
+        newEnemy.transform.position = spawnTransform.position;
+        newEnemy.transform.rotation = spawnTransform.rotation;
+        _enemiesLeftToSpawn -= 1;
+    }
+}
