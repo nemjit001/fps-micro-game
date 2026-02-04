027256401f4da56cf65d4f95975e88cefb363827
Tijmen Verhoef <tijmenmenno@gmail.com>
2026-02-04 16:26:31 +0100

    fixes data binding paths

--- Assets/Scripts/UI/UIManagers/SettingsMenuManagers/InputSettingsUIManager.cs
+++ Assets/Scripts/UI/UIManagers/SettingsMenuManagers/InputSettingsUIManager.cs
@@ -1,18 +1,37 @@
 using System;
+using System.IO;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class InputSettingsUIManager : UIManager
 {
-    public Action OnLeaveMenu = null;
+    [SerializeField]
+    PlayerInputSettings _inputSettings = null;
 
+    const string INPUT_SETTINGS_FILENAME = "input_settings.json";
+
+    public Action OnLeaveMenu = null;
     Button _backButton = null;
     
     void OnEnable()
     {
         UIDocument document = GetComponent<UIDocument>();
-        _backButton = document.rootVisualElement.Query<Button>("back-button");
+        document.rootVisualElement.dataSource = _inputSettings;
 
+        Slider lookSensSlider = document.rootVisualElement.Query<Slider>("look-sens-slider");
+        lookSensSlider.SetBinding("value", new DataBinding {
+            dataSourcePath = new Unity.Properties.PropertyPath(nameof(PlayerInputSettings.lookSensitivity))
+        });
+
+        Toggle invertYLookToggle = document.rootVisualElement.Query<Toggle>("invert-y-look-toggle");
+        invertYLookToggle.SetBinding("value", new DataBinding {
+            dataSourcePath = new Unity.Properties.PropertyPath(nameof(PlayerInputSettings.invertYLook))
+        });
+
+        _backButton = document.rootVisualElement.Query<Button>("back-button");
         _backButton.clicked += OnPressBackButton;
+
+        loadInputSettings();
     }
 
     void OnDisable()
@@ -22,6 +41,23 @@ public class InputSettingsUIManager : UIManager
 
     private void OnPressBackButton()
     {
+        SaveInputSettings();
         OnLeaveMenu();
+    }
+
+    void loadInputSettings()
+    {
+        string filePath = $"{Application.persistentDataPath}/{INPUT_SETTINGS_FILENAME}";
+        if (File.Exists(filePath))
+        {
+            string contents = File.ReadAllText(filePath);
+            JsonUtility.FromJsonOverwrite(contents, _inputSettings);
+        }
+    }
+
+    private void SaveInputSettings()
+    {
+        string jsonData = JsonUtility.ToJson(_inputSettings);
+        File.WriteAllText($"{Application.persistentDataPath}/{INPUT_SETTINGS_FILENAME}", jsonData);
     }
 }
